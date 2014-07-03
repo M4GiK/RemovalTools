3aada19069e6d7936c269a8e226dd2326c7494c3
m4gik <michal.szczygiel@wp.pl>
2014-07-03 23:12:42 +0200

    Fix typo with project name

---
+++ removal-tool/src/main/java/com/m4gik/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package com.m4gik;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
