b34c61c806bf0042f6b97062b87e6552a1a28bf6
m4gik <michal.szczygiel@wp.pl>
2014-07-05 19:53:28 +0200

    Update Constants class

--- removal-tool/src/main/java/com/m4gik/Constants.java
+++ removal-tool/src/main/java/com/m4gik/Constants.java
@@ -4,10 +4,14 @@ package com.m4gik;
  * Contains the client IDs and scopes for allowed clients consuming your API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client ID";
-  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
-  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
-  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 
-  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+    public static final String ADDRESS = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&start=4&q=";
+
+    public static final String CHARSET = "UTF-8";
+
+    public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+
+    public static final String GOOGLE = "http://www.google.com/search?q=";
+
+    public static final String WEB_CLIENT_ID = "replace this with your web client ID";
 }
